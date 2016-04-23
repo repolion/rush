751445b2abdf6fb82481020ed4428388ce2aa782
Liongeek <Liongeek@192.168.0.4>
2016-04-23 22:10:29 +0200

    test 1.876.578

--- RushHour/src/g42442/view/Json.java
+++ RushHour/src/g42442/view/Json.java
@@ -32,7 +32,7 @@ public class Json {
         try {
            
             Object obj = parser.parse(new FileReader(
-                    "./file" + dice + ".txt"));
+                    "file" + dice + ".txt"));
        
            
             
@@ -70,7 +70,7 @@ public class Json {
         try {
 
             Object obj = parser.parse(new FileReader(
-                    "./file" + dice + ".txt"));
+                    "file" + dice + ".txt"));
 
             JSONObject jsonObject = (JSONObject) obj;
             String nbCars = (String) jsonObject.get("cars");
